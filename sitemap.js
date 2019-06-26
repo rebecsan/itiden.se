49c20749b7027fba6794b92d10fd3742a21959a6
Daniel Brodin <daniel@itiden.se>
2019-06-26 12:13:10 +0200

    Add menu options to sitemap

--- sitemap.js
+++ sitemap.js
@@ -1,7 +1,12 @@
 const sm = require('sitemap');
 const cases = require('./data/data/case.json');
+const menu = require('./data/data/menu.json');
 
-const urls = [{ url: '/' }, { url: '/kontakt' }];
+const urls = [{ url: '/' }];
+
+menu.forEach(m => {
+  urls.push({ url: `/${m.slug}` });
+});
 
 cases.forEach(caseData => {
   urls.push({
