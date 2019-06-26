70a8cf5eb31d951de7a7bbe192e173218ec689f1
Daniel Brodin <daniel@itiden.se>
2019-06-26 12:03:41 +0200

    Add soft newline support

---
+++ components/Contentful/Document.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import {
+  documentToReactComponents,
+  Options,
+} from '@contentful/rich-text-react-renderer';
+import { Document as DocumentType, BLOCKS } from '@contentful/rich-text-types';
+import { ProfileCard } from '../ProfileCard';
+import { Employee } from '../../models';
+import {
+  getEmbeddedContentType,
+  getEmbeddedFields,
+} from '../../helpers/contentHelpers';
+
+interface DocumentProps {
+  content: DocumentType;
+}
+
+const documentOptions: Options = {
+  renderNode: {
+    [BLOCKS.EMBEDDED_ENTRY]: node => {
+      if (getEmbeddedContentType(node) === 'employee') {
+        const data = getEmbeddedFields<Employee>(node);
+        return <ProfileCard {...data} />;
+      }
+
+      return null;
+    },
+  },
+  renderText: text => {
+    return text.split('\n').reduce(
+      (children, textSegment, index) => {
+        return [...children, index > 0 && <br key={index} />, textSegment];
+      },
+      [] as React.ReactNode[]
+    );
+  },
+};
+
+export const Document: React.FC<DocumentProps> = ({ content }) => {
+  return <>{documentToReactComponents(content, documentOptions)}</>;
+};
--- pages/page.tsx
+++ pages/page.tsx
@@ -1,45 +1,23 @@
-import {
-  documentToReactComponents,
-  Options,
-} from '@contentful/rich-text-react-renderer';
-import { BLOCKS } from '@contentful/rich-text-types';
 import { withAmp } from 'next/amp';
 import Head from 'next/head';
+import { withRouter, WithRouterProps } from 'next/router';
 import React from 'react';
 import tw from 'tailwind.macro';
-import { Header, HeaderContent, Page, Content } from '../components/Layout';
+import { Document } from '../components/Contentful';
+import { Content, Header, HeaderContent, Page } from '../components/Layout';
 import { Body, Title } from '../components/Typography';
 import { getPage } from '../data/page';
-import {
-  getEmbeddedContentType,
-  getEmbeddedFields,
-} from '../helpers/contentHelpers';
-import { Employee } from '../models/Employee';
-import { ProfileCard } from '../components/ProfileCard';
-import { withRouter, WithRouterProps } from 'next/router';
 
 interface PageRouterProps {
   slug: string;
 }
 
-const documentOptions: Options = {
-  renderNode: {
-    [BLOCKS.EMBEDDED_ENTRY]: node => {
-      if (getEmbeddedContentType(node) === 'employee') {
-        const data = getEmbeddedFields<Employee>(node);
-        return <ProfileCard {...data} />;
-      }
-
-      return null;
-    },
-  },
-};
-
 const PagePage: React.FC<WithRouterProps<PageRouterProps>> = props => {
   const { slug = '' } = (props.router && props.router.query) || {};
   const page = getPage(slug);
+
   if (!page) {
-    return '404';
+    return <div>404</div>;
   }
 
   const { title, header, body } = page;
@@ -55,7 +33,7 @@ const PagePage: React.FC<WithRouterProps<PageRouterProps>> = props => {
               ${tw`mt-4`}
             `}
           >
-            {documentToReactComponents(header, documentOptions)}
+            <Document content={header} />
           </Body>
         </HeaderContent>
       </Header>
@@ -65,7 +43,7 @@ const PagePage: React.FC<WithRouterProps<PageRouterProps>> = props => {
             ${tw`mt-4`}
           `}
         >
-          {documentToReactComponents(body, documentOptions)}
+          <Document content={body} />
         </Body>
       </Content>
     </Page>
