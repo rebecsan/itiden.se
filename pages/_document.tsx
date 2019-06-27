f154aa0b9afa12439f5c93395aff264a484bc554
danielbrodin <hej@danielbrodin.se>
2019-06-27 10:23:52 +0200

    Add favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -40,6 +40,7 @@ export default class MyDocument extends Document {
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta name="theme-color" content="#C75000" />
+          <link rel="shortcut icon" type="image/x-icon" href="/static/favicon.ico" />
           {this.props.styles}
           <link
             href="https://fonts.googleapis.com/css?family=Nunito:400,600,700|Ubuntu+Mono:700&display=swap"
