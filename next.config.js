408f5d19c106a59bdc391e7ca73f712d3d7e74b8
danielbrodin <hej@danielbrodin.se>
2019-06-27 09:41:24 +0200

    Static html export

--- next.config.js
+++ next.config.js
@@ -2,11 +2,26 @@ const withTypescript = require('@zeit/next-typescript');
 const withCSS = require('@zeit/next-css');
 const withOptimizedImages = require('next-optimized-images');
 
+const pages = require('./data/data/page.json');
+const cases = require('./data/data/case.json');
+
 module.exports = withCSS(
   withOptimizedImages(
     withTypescript({
-      // amp: true,
-      exportPathMap: 'out'
+      exportPathMap: async function() {
+        const paths = {
+          '/': { page: '/' },
+        }
+
+        pages.forEach(page => {
+          paths[`/${page.slug}`] = { page: '/page' };
+        });
+        cases.forEach(c => {
+          paths[`/case/${c.slug}`] = { page: '/case' };
+        });
+
+        return paths;
+      }
     })
   )
 );
