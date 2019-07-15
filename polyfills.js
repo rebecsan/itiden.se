65f31414f9d68136145f8db8b2fd17269474123d
Andreas Bergqvist <andreas@itiden.se>
2019-07-15 22:30:07 +0200

    Add IE polyfills

--- polyfills.js
+++ polyfills.js
@@ -3,4 +3,6 @@ import 'core-js/features/array/find';
 import 'core-js/features/array/fill';
 import 'core-js/features/object/entries';
 import 'core-js/features/object/assign';
+import 'core-js/features/string/starts-with';
+import 'core-js/features/string/includes';
 import 'lazysizes/plugins/respimg/ls.respimg';
