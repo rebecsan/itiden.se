a1927bf6d5e2e6d6862ae6b8568575a4913c7336
Daniel Brodin <daniel@itiden.se>
2019-08-13 16:20:20 +0200

    Add script for analyzing bundle

--- next.config.js
+++ next.config.js
@@ -2,73 +2,78 @@ const withCSS = require('@zeit/next-css');
 const withOptimizedImages = require('next-optimized-images');
 const Dotenv = require('dotenv-webpack');
 const path = require('path');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
 
 const pages = require('./data/data/page.json');
 const cases = require('./data/data/case.json');
 
-module.exports = withCSS(
-  withOptimizedImages({
-    exportTrailingSlash: true,
-    optimizeImages: false,
-    exportPathMap: async function() {
-      const paths = {
-        '/': { page: '/' }
-      };
+module.exports = withBundleAnalyzer(
+  withCSS(
+    withOptimizedImages({
+      exportTrailingSlash: true,
+      optimizeImages: false,
+      exportPathMap: async function() {
+        const paths = {
+          '/': { page: '/' },
+        };
 
-      pages
-        .filter(page => page.slug !== '/')
-        .forEach(page => {
-          paths[`/${page.slug}`] = {
-            page: '/page',
-            query: { slug: page.slug }
+        pages
+          .filter(page => page.slug !== '/')
+          .forEach(page => {
+            paths[`/${page.slug}`] = {
+              page: '/page',
+              query: { slug: page.slug },
+            };
+          });
+        cases.forEach(c => {
+          paths[`/case/${c.slug}`] = {
+            page: '/case',
+            query: { slug: c.slug },
           };
         });
-      cases.forEach(c => {
-        paths[`/case/${c.slug}`] = {
-          page: '/case',
-          query: { slug: c.slug }
+
+        return paths;
+      },
+      webpack: config => {
+        config.plugins = config.plugins || [];
+
+        config.plugins = [
+          ...config.plugins,
+
+          // Read the .env file
+          new Dotenv({
+            path: path.join(__dirname, '.env'),
+            systemvars: true,
+          }),
+        ];
+
+        config.resolve.alias = {
+          ...config.resolve.alias,
+          'react-spring$': require.resolve('react-spring/web.cjs'),
+          'react-spring/renderprops$': require.resolve(
+            'react-spring/renderprops.cjs'
+          ),
+          // 'react-use-gesture': require.resolve('react-use-gesture/web.cjs')
         };
-      });
 
-      return paths;
-    },
-    webpack: config => {
-      config.plugins = config.plugins || [];
+        const originalEntry = config.entry;
+        config.entry = async () => {
+          const entries = await originalEntry();
 
-      config.plugins = [
-        ...config.plugins,
+          if (
+            entries['main.js'] &&
+            !entries['main.js'].includes('./polyfills.js')
+          ) {
+            entries['main.js'].unshift('./polyfills.js');
+          }
 
-        // Read the .env file
-        new Dotenv({
-          path: path.join(__dirname, '.env'),
-          systemvars: true
-        })
-      ];
+          return entries;
+        };
 
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        'react-spring$': require.resolve('react-spring/web.cjs'),
-        'react-spring/renderprops$': require.resolve(
-          'react-spring/renderprops.cjs'
-        ),
-        'react-use-gesture': require.resolve('react-use-gesture/web.cjs')
-      };
-
-      const originalEntry = config.entry;
-      config.entry = async () => {
-        const entries = await originalEntry();
-
-        if (
-          entries['main.js'] &&
-          !entries['main.js'].includes('./polyfills.js')
-        ) {
-          entries['main.js'].unshift('./polyfills.js');
-        }
-
-        return entries;
-      };
-
-      return config;
-    }
-  })
+        return config;
+      },
+    })
+  )
 );
