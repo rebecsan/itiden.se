24e54762f5e2ff6bdd53869786aa74c2025400a5
Andreas Bergqvist <andreas@itiden.se>
2019-09-08 09:17:23 +0200

    Fix font src

--- script/headers.js
+++ script/headers.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 async function generateHeadersFile() {
   const header = `/*
-  Content-Security-Policy: default-src 'none'; img-src 'self' https://images.ctfassets.net data:; style-src 'self' https://fonts.googleapis.com; font-src: https://fonts.gstatic.com; script-src 'self' https://www.google-analytics.com
+  Content-Security-Policy: default-src 'none'; img-src 'self' https://images.ctfassets.net data:; style-src 'self' https://fonts.googleapis.com; font-src https://fonts.gstatic.com; script-src 'self' https://www.google-analytics.com
   X-Xss-Protection: 1; mode=block
   Strict-Transport-Security: max-age=63072000; includeSubDomains; preload
   X-Frame-Options: SAMEORIGIN
