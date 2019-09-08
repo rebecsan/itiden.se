8f3a2aac3b27d2edd34745194b50b6f4ef7d5ab0
Andreas Bergqvist <andreas@itiden.se>
2019-09-08 09:01:59 +0200

    Update Content-Security-Policy

--- script/headers.js
+++ script/headers.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 async function generateHeadersFile() {
   const header = `/*
-  Content-Security-Policy: default-src 'self' https://www.google-analytics.com https://images.ctfassets.net https://fonts.googleapis.com 'unsafe-inline'
+  Content-Security-Policy: default-src 'none'; img-src 'self' https://images.ctfassets.net; style-src 'self'; font-src: https://fonts.gstatic.com; script-src 'self' https://www.google-analytics.com
   X-Xss-Protection: 1; mode=block
   Strict-Transport-Security: max-age=63072000; includeSubDomains; preload
   X-Frame-Options: SAMEORIGIN
