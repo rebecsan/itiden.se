35c8b57586bf904c40dd34ec7b71caa9085c9374
Andreas Bergqvist <andreas@itiden.se>
2019-09-08 08:53:55 +0200

    Add contentful and googlefonts assets to headers

--- script/headers.js
+++ script/headers.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 async function generateHeadersFile() {
   const header = `/*
-  Content-Security-Policy: default-src 'self' https://www.google-analytics.com 'unsafe-inline'
+  Content-Security-Policy: default-src 'self' https://www.google-analytics.com https://images.ctfassets.net https://fonts.googleapis.com 'unsafe-inline'
   X-Xss-Protection: 1; mode=block
   Strict-Transport-Security: max-age=63072000; includeSubDomains; preload
   X-Frame-Options: SAMEORIGIN
