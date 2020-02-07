6d83e10c90f276b52d25c53d2421023d6752a949
Andreas Bergqvist <andreas@itiden.se>
2020-02-07 17:30:47 +0100

    Startpage styling changes

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -2,5 +2,5 @@ import styled from 'styled-components';
 import tw from 'tailwind.macro';
 
 export const Header = styled.div`
-  ${tw`pt-6 pb-12 md:pb-16 md:pt-16`}
+  ${tw`pt-6 pb-12 md:pb-32 md:pt-56`}
 `;
