54cdb1032bf928ce748b83c20c04e526c82c44be
Andreas Bergqvist <andreas@itiden.se>
2020-02-11 07:37:19 +0100

    Add support for Fathom analytics

--- components/Analytics/Analytics.tsx
+++ components/Analytics/Analytics.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactGA from 'react-ga';
 import Router from 'next/router';
+import * as Fathom from 'fathom-client';
 
-function initGA() {
+function initAnalytics() {
   if (process.env.GA) {
     ReactGA.initialize(process.env.GA);
+  }
+  if (process.env.FATHOM) {
+    Fathom.load();
+    Fathom.setSiteId(process.env.FATHOM);
   }
 }
 
@@ -13,13 +18,16 @@ function logPageView() {
     ReactGA.set({ page: window.location.pathname });
     ReactGA.pageview(window.location.pathname);
   }
+  if (process.env.FATHOM) {
+    Fathom.trackPageview();
+  }
 }
 
 export const Analytics: React.FC<{}> = () => {
   React.useEffect(() => {
-    if (!window.GA_INITIALIZED) {
-      initGA();
-      window.GA_INITIALIZED = true;
+    if (!window.ANALYTICS_INITIALIZED) {
+      initAnalytics();
+      window.ANALYTICS_INITIALIZED = true;
     }
 
     logPageView();
