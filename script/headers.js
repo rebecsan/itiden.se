600bd3442b644b9f112d596ee25002811e1c0635
Andreas Bergqvist <andreas@itiden.se>
2020-02-11 07:47:42 +0100

    Update headers

--- script/headers.js
+++ script/headers.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 async function generateHeadersFile() {
   const header = `/*
-  Content-Security-Policy: default-src 'none'; img-src 'self' https://images.ctfassets.net http://www.google-analytics.com data:; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src https://fonts.gstatic.com; script-src 'self' https://www.google-analytics.com; connect-src 'self' https://www.google-analytics.com
+  Content-Security-Policy: default-src 'none'; prefetch-src 'self'; img-src 'self' https://images.ctfassets.net http://www.google-analytics.com data:; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src https://fonts.gstatic.com; script-src 'self' https://cdn.usefathom.com https://www.google-analytics.com; connect-src 'self' https://www.google-analytics.com
   X-Xss-Protection: 1; mode=block
   Strict-Transport-Security: max-age=63072000; includeSubDomains; preload
   X-Frame-Options: SAMEORIGIN
