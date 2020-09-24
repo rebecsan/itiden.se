6d1aef1feca08c60b1d8a75798b3fc4ffa8cf43b
rebecsan <rebecca.sandelin@gmail.com>
2020-09-24 14:40:34 +0200

    Add custom colors to tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,21 +3,39 @@ module.exports = {
     fontFamily: {
       body: '"Open Sans", sans-serif',
     },
-    boxShadow: {
-      md: '0px 4px 4px rgba(0, 0, 0, 0.2)',
-    },
     extend: {
       colors: {
-        'brand': '#504DD2',
-        'gray-f0': '#F0F0F0',
-        'gray-74': '#747474',
-        'gray-4b': '#4b4a4f',
-        'gray-24': '#242424',
-        'blue-f5': '#F5F8FF',
+        gray: {
+          100: '#F4F4F4',
+          200: '#EBEBEB',
+          250: '#E0E0E0',
+          300: '#D6D6D6',
+          350: '#C2C2C2',
+          400: '#808080',
+          500: '#505050',
+          600: '#2D2D2D',
+          700: '#242424',
+          800: '#181818',
+          900: '#0C0C0C',
+        },
+        purple: {
+          700: '#504DD2',
+        },
+        teal: {
+          400: '#5CFFF5',
+        },
+        green: '#5CFF8A',
+        red: '#D24D4D',
+        yellow: '#FFDB5C',
       },
       fill: {
         logo: '#242424',
       },
     },
+  },
+  variants: {},
+  plugins: [],
+  future: {
+    removeDeprecatedGapUtilities: true,
   },
 };
