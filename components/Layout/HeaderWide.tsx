74debb0fadb7ea112ec507c0b39c420033e2a93d
Beatrice Svensson <beatrice@itiden.se>
2020-10-01 10:16:14 +0200

    Center header content horizontally

--- components/Layout/HeaderWide.tsx
+++ components/Layout/HeaderWide.tsx
@@ -4,9 +4,8 @@ import tw from 'twin.macro';
 export const HeaderWide = styled.div`
   ${tw`flex flex-col justify-center`}
   height: 100vh;
+  margin-top: -85px;
   @media (max-width: 767px) {
-    display: flex;
-    flex-direction: column;
     justify-content: flex-end;
     margin-top: -143px;
   }
