1b0625e13de9b5f6ebdfdda4e0e435a991150db9
rebecsan <rebecca.sandelin@gmail.com>
2020-10-02 16:31:00 +0200

    renamed page labs to om

--- pages/om.tsx
+++ pages/om.tsx
@@ -1,49 +1,38 @@
 import { NextComponentType } from 'next';
 import React from 'react';
-import styled from 'styled-components';
-import tw from 'twin.macro';
+import { CaseGrid } from '../components/Case';
 import { IndexHeader } from '../components/IndexHeader';
-import { AboutIconsGroup } from '../components/AboutIcons';
-import { Header, Page, Content, Paragraph, Hero } from '../components/Layout';
+import { Header, Page, Content } from '../components/Layout';
+import { Case } from '../models';
 
-const ContentWrapper = styled.div`
-  ${tw`md:hidden`};
-  padding-top: ${(props: { paddingTop: string }) => props.paddingTop};
-  padding-bottom: ${(props: { paddingBottom: string }) => props.paddingBottom};
-`;
+interface IndexPageProps {
+  cases: Case[];
+}
 
-const GrayBanner = styled(Hero)`
-  ${tw`bg-gray-700 pt-16 pb-1 md:pt-32`}
-`;
-
-const IndexPage: NextComponentType<{}, {}> = () => {
+const IndexPage: NextComponentType<{}, {}, IndexPageProps> = ({ cases }) => {
   return (
     <Page>
-      <IndexHeader title="Om" />
+      <IndexHeader title="Labs" />
       <Header role="banner">
-        <ContentWrapper paddingTop={'16rem'} paddingBottom={'0.5rem'}>
-          <Content>
-            <h3>Kunskap genom insikt och erfarenhet</h3>
-            <Paragraph>
-              För att ett projekt – vare sig det gäller för appar, webb eller
-              andra tekniska lösningar – skall bli så bra som möjligt krävs
-              insikter. Därför tycker vi det är viktigt att vara med tidigt i
-              processen, gärna redan vid idéstadiet. Genom insikt och förståelse
-              för dina behov anpassar vi teknik och väg framåt. Vi utgår inte
-              från mallar utan skräddarsyr varje ensklit projekt för att nå
-              bästa möjliga resultat. Vi jobbar transparent och under projektets
-              gång är du delaktig hela vägen i produktionen fram till färdig
-              produkt. På detta vis kan vi ta snabba beslut om förutsättningar
-              för projektet förändras och vi kan tillsammans nå önskat resultat.
-            </Paragraph>
-          </Content>
-        </ContentWrapper>
+        <Content>
+          <h3>Labs, Itiden</h3>
+          <h1>Utveckling + nöje</h1>
+          <p>
+            Det händer ibland att vi bygger egna produkter eller tjänster på
+            Itiden. Det kan vara för att utmana oss, lära oss nya tekniker eller
+            helt enkelt för att vi tycker produkten behövs. Denna utveckling
+            samlar vi under vad vi kallar Itiden Labs.
+          </p>
+        </Content>
       </Header>
-      <GrayBanner>
-        <AboutIconsGroup />
-      </GrayBanner>
+      <CaseGrid cases={cases.filter(c => c.labs)} />
     </Page>
   );
+};
+
+IndexPage.getInitialProps = async () => {
+  const cases = await import('../data/data/case.json').then(m => m.default);
+  return { cases: cases.filter(c => c.labs) };
 };
 
 export default IndexPage;
